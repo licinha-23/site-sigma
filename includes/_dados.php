a6b057fc6a6769e886b219e06f04aeff7c2aa920
yL0pes <92817220+licinha-23@users.noreply.github.com>
2022-06-09 12:17:55 -0300

    commit 09/06

---
+++ includes/_dados.php
@@ -0,0 +1,17 @@
+<?php
+
+$produtos = array (
+    0 => array ('nome'=>'capivara-legal','preço'=>'1.200','descrição'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'),
+    1 => array ('nome'=>'anta-legal','preço'=>'1.200','descrição'=>'Descrição Anta Legal','imagem'=>'anta-legal.jpg'),
+    2 => array ('nome'=>'tamandua-legal','preço'=>'1.200','descrição'=>'Descrição Tamandua Legal','imagem'=>'tamandua-legal.jpg'),
+    3 => array ('nome'=>'ararinha-legal','preço'=>'1.200','descrição'=>'Descrição Ararinha Azul Legal','imagem'=>'ararinha-legal.jpg'),
+    4 => array ('nome'=>'mico-legal','preço'=>'1.200','descrição'=>'Descrição Mico Leão Dourado Legal','imagem'=>'mico-legal.jpg'),
+    5 => array ('nome'=>'boto-legal','preço'=>'1.200','descrição'=>'Descrição Boto Cor de Rosa Legal','imagem'=>'boto-legal.jpg'),
+    6 => array ('nome'=>'tatu-legal','preço'=>'1.200','descrição'=>'Descrição Tatu Bolinha Legal','imagem'=>'tatu-legal.jpg'),
+    7 => array ('nome'=>'tucano-legal','preço'=>'1.200','descrição'=>'Descrição Tucano Legal','imagem'=>'tucano-legal.jpg'),
+    8 => array ('nome'=>'onca-legal','preço'=>'1.200','descrição'=>'Descrição Onça Pintada Legal','imagem'=>'onca-legal.jpg'),
+    9 => array ('nome'=>'caramelo-legal','preço'=>'1.200','descrição'=>'Descrição Doguinho Caramelo Legal','imagem'=>'caramelo-legal.jpg'),
+ 
+);
+
+?>
--- index.php
+++ index.php
@@ -8,6 +8,13 @@ include_once './includes/_header.php';
 <h1>Home</h1>
 
 <?php
+for ($i=0; $i < 3; $i++ ) 
+    echo '<img src="./content/'.$produtos [$i] ['imagem']. '">';
+    echo $produtos [$i] ['nome'];
+    echo $produtos [$i] ['preço'];
+?>
+
+<?php
 // include do footer
 include_once './includes/_footer.php';
 ?>
